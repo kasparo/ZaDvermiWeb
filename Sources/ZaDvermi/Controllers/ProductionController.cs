7a1697d17620265f83bb1adde6f97ff300b40eb3
Ondrej Kaspar <kaspar.ondrej@volny.cz>
2015-04-03 19:05:16 +0200

    Fixing

---
+++ Sources/ZaDvermi/Controllers/ProductionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZaDvermi.Models;
+
+namespace ZaDvermi.Controllers
+{
+    public class ProductionController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator,Manager,Editor")]
+        public ActionResult Add()
+        {
+            var productionItem = new Article();
+            return View("Edit", productionItem);
+        }
+    }
+}
